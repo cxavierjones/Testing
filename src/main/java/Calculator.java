f48608c5d48afd76e1023ee47b3a0f1a6a2b21ad
Christopher Jones <cxavierjones@hotmail.com>
2022-04-15 11:55:47 -0400

    junit test cases

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -14,4 +14,21 @@ public class Calculator {
         }
         return mul;
     }
+    public Integer max(Integer[] arr){
+        int maximum = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if (maximum < arr[i]) {
+                maximum = arr[i];
+            }
+        }
+        return maximum;
+    }
+    public double divide (double num, double den){
+        if(den==0)
+            throw new ArithmeticException("Division by 0 is not allowed");
+        /*
+        Exception is used when you are dealing with some unusual case
+         */
+        return num/den;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,79 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void setup(){
+        calculator=new Calculator();
+    }
     @Test
     void TestSum(){
-        Calculator calculate = new Calculator();
-        Integer sum = calculate.sum(new Integer[]{3,2,1});
+        Integer sum = calculator.sum(new Integer[]{3,2,1});
         assertEquals(6,sum);
     }
     @Test
     void TestSum2(){
-        Calculator calculate = new Calculator();
-        Integer sum = calculate.sum(new Integer[]{3,2,1,10,10});
+        Integer sum = calculator.sum(new Integer[]{3,2,1,10,10});
         assertEquals(26,sum);
     }
 
     @Test
     void TestMultiplication(){
-        Calculator calculate = new Calculator();
-        Integer multiply = calculate.multiply(new Integer[]{4,2});
+        Integer multiply = calculator.multiply(new Integer[]{4,2});
         assertEquals(8,multiply);
     }
 
     @Test
     void TestMultiplication2(){
-        Calculator calculate = new Calculator();
-        Integer multiply = calculate.multiply(new Integer[]{4,2,5,3});
+        Integer multiply = calculator.multiply(new Integer[]{4,2,5,3});
         assertEquals(120,multiply);
     }
+
+    @Test
+    void TestMax(){
+        Integer max = calculator.max(new Integer[]{3,4,6,5,7});
+        assertEquals(7,max);
+    }
+
+    @Test
+    void TestMax2(){
+        Integer max = calculator.max(new Integer[]{-4,-3,-6,-5,-7});
+        assertEquals(-3,max);
+    }
+    @Test
+    void testDivide(){
+        double div = calculator.divide(4,2);
+        assertEquals(2,div);
+    }
+    @Test
+    void testDivide2(){
+        double div = calculator.divide(2,4);
+        assertEquals(0.5,div);
+    }
+//    @Test
+//    void testDivide3(){
+//        double div = calculator.divide(2,0);
+//        assertEquals(Double.POSITIVE_INFINITY,div);
+//    }
+
+       @Test
+    void testDivide3(){
+        // lambda expression
+        assertThrows(ArithmeticException.class,()->calculator.divide(2,0));
+    }
+
+    @Test
+    void testDivide4(){
+        assertThrows(ArithmeticException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                calculator.divide(5,0);
+            }
+        });
+    }
 }
+
